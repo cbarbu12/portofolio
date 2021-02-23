8f42b0e7a98944a69d948aed3f88b0105ee0de32
Cristian Barbu <61808424+cbarbu12@users.noreply.github.com>
2021-02-24 00:05:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -37,24 +37,24 @@ def submit_form():
     else:
         return 'something went wrong!'
 
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
-#
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-#
-# @app.route('/components.html')
-# def comp():
-#     return render_template('components.html')
-#
-# @app.route('/work.html')
-# def work():
-#     return render_template('work.html')
-#
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/components.html')
+def comp():
+    return render_template('components.html')
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
 
 
